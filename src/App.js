7b47497b2068b408288498d6026ee01dac0ee73d
Tanvir <tanvirahmed1418@gmail.com>
2022-08-30 12:14:59 +0600

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-
-import Footer from "./components/Common/Footer";
-import Home from "./components/Home";
-import Header from "./components/Common/Header";
-import AboutUs from "./components/AboutUs";
-import MessageAlert from "./components/Common/MessageAlert";
-
 import "./Styles.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-      </Routes>
-      <Footer />
-      <MessageAlert />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
